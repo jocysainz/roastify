9aa0ce2752391b3b0d05e9ce9fe5284f3d99ec9e
jocysainz <lauragrajeda29@yahoo.com>
2025-12-06 16:46:53 -0600

    added the callback.html

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,5 @@
 const clientId = "0af19046848d4406a95c670c0257af84"; /*change this later*/
-const redirectUri = "https://finalwwd330.netlify.app/";/*change this later too, working on netlify*/
+const redirectUri = "https://finalwwd330.netlify.app/callback.html";/*change this later too, working on netlify*/
 
 const scopes = [
   "user-read-private",
